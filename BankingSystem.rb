fcfa87d2418ef2306546fa3669f5d674d798545a
JoshuaMonyei <monyeijosh01@gmail.com>
2020-05-10 12:30:37 +0100

    Major improvements

--- BankingSystem.rb
+++ BankingSystem.rb
@@ -6,9 +6,9 @@ def root
       3. Close App: ")
   login_page = gets.chomp
   if login_page == '1'
-    puts('Please input your username: ')
+    print('Please input your staff username: ')
     username = gets.chomp
-    puts('Please input your password: ')
+    print('Please input your password: ')
     password = gets.chomp
     # opening staff.txt file to verify user input
     file = File.open('staff.txt', 'r')
@@ -25,21 +25,11 @@ def root
     end
 
   elsif login_page == '2'
-    puts('What would you like to do: Enter 1 or 2 to choose
-      1. Create an account pin
-      2. Login')
-    options = gets.chomp
-    if options == '1'
-      create_customer_pin
-    elsif options == '2'
-
-    else
-      puts('Invalid entry')
-      root
-    end
+    customer_options
 
   elsif login_page == '3'
     exit
+
   else
     puts('Invalid entry')
     # recalling the root method after an invalid entry
@@ -59,7 +49,7 @@ def staff_home_page
   if staff_options == '1'
     print('Account name: ')
     acct_name = gets.chomp
-    print('Opening Balance: ')
+    print('Opening Balance: $')
     opening_bal = gets.chomp
     print('Account type: ')
     acct_type = gets.chomp
@@ -71,11 +61,11 @@ def staff_home_page
     # creating dictionaries to save in the customer.txt file
     overall_data = {}
     banking_details = {
-      'Account name': acct_name, 'Opening Balance': opening_bal + '$',
+      'Account name': acct_name, 'Opening Balance': opening_bal.to_i,
       'Account type': acct_type, 'Account email': acct_email
     }
     overall_data[acct_num] = banking_details
-    # check if customer.txt is empty by doing this, the essence of this is to enable the smooth reading of text from the file.
+    # checking if customer.txt is empty, the essence of this is to allow reading of text from the file.
     if File.zero?('customer.txt')
       File.open('customer.txt', 'w') do |f|
         JSON.dump(overall_data, f)
@@ -87,7 +77,7 @@ def staff_home_page
         # Then append the present user details
         overall_data[acct_num] = banking_details
       end
-      # Then open the customer file in write mode and dump the overall_data to it
+      # opening the customer.txt file in write mode to dump the overall_data to it.
       File.open('customer.txt', 'w') do |fil|
         JSON.dump(overall_data, fil)
       end
@@ -103,7 +93,7 @@ def staff_home_page
     if data.key?(check_acct)
       puts("\n\tAccount Found ! See details below;")
       data[check_acct].each do |key, value|
-        puts key.to_s + ' : ' + value
+        puts key.to_s + ' : ' + value.to_s
       end
       staff_home_page
     else
@@ -121,31 +111,138 @@ def staff_home_page
   end
 end
 
+def customer_options
+  puts('What would you like to do: Enter 1 or 2 to choose;
+    1. Create an account pin
+    2. Login
+    3. Exit')
+  options = gets.chomp
+  if options == '1'
+    create_customer_pin
+  elsif options == '2'
+    customer_login
+  elsif options == '3'
+    root
+  else
+    puts('Invalid entry')
+    customer_options
+  end
+end
+
 def create_customer_pin
+  require 'json'
   print('If you have an account with us please enter your account number: ')
   customer_acct = gets.chomp
-  File.open('customer.txt', 'r') do |file|
-    if file.read.include? customer_acct
-      puts('Account number is Valid')
-      print('Create an account pin: ')
-      acct_pin = gets.chomp
-      # checking only numbers were entered as input and the characters are between 4 - 6
-      if acct_pin.to_i.to_s == acct_pin && acct_pin.length >= 4 && acct_pin.length <= 6
-        puts(acct_pin)
-        File.open('customer_login.txt', 'a') do |file|
-          file.write('Account number: ' + customer_acct + ' ')
-          file.write('Account Pin: ' + acct_pin + "\n")
+  # Checking to see customer input exist in the data base
+  f_obj = File.open('customer.txt')
+  data = JSON.load(f_obj)
+  if data.key?(customer_acct)
+    puts("\n\tAccount number is valid")
+    print('Create an account pin: ')
+    acct_pin = gets.chomp
+    # checking only numbers were entered as input and the characters are between 4 - 6
+    if acct_pin.to_i.to_s == acct_pin && acct_pin.length >= 4 && acct_pin.length <= 6
+      customer_login_details = {}
+      entry = {'Account pin': acct_pin}
+      customer_login_details[customer_acct] = entry
+      # checking if customer_login.txt is empty, the essence of this is to enable us have access to file through the JSON module.
+      if File.zero?('customer_login.txt')
+        File.open('customer_login.txt', 'w') do |f|
+          JSON.dump(customer_login_details, f)
         end
-        root
-
       else
-        puts('Pin Should be between 4-6 digits')
-        create_customer_pin
+        # i.e. if the customer-login.txt is not empty
+        File.open('customer_login.txt') do |filn|
+          customer_login_details = JSON.load(filn)
+          # Then append the present user details
+          customer_login_details[customer_acct] = entry
+        end
+        # Opening the customer_login file in write mode to dump the customer login details.
+        File.open('customer_login.txt', 'w') do |fil|
+          JSON.dump(customer_login_details, fil)
+        end
       end
+      customer_options
+
     else
-      puts('Invalid account number')
+      puts('Pin Should be between 4-6 digits')
       create_customer_pin
     end
+  else
+    puts('Invalid account number')
+    create_customer_pin
+  end
+end
+
+def customer_login
+  require 'json'
+  print('Please enter your Account Number: ')
+  customer_acct = gets.chomp
+  print('Enter Account Pin: ')
+  acct_pin = gets.chomp
+  f_obj = File.open('customer_login.txt')
+  data = JSON.load(f_obj)
+  login = true
+  if data.key?(customer_acct) && data[customer_acct]['Account pin'] == acct_pin
+   puts("\n\tLogin Successful")
+   while login == true
+    puts("What would you like to do: Enter 1, 2 or 3 to choose
+     1. Check Account Balance
+     2. Make Deposit
+     3. Withdrawals
+     4. Logout")
+    customer_login_options = gets.chomp
+    if customer_login_options == '1'
+     fil = File.open('customer.txt')
+     customer_details = JSON.load(fil)
+     puts("\n\tYour account balance is: " + customer_details[customer_acct]['Opening Balance'].to_s)
+ 
+    elsif customer_login_options == '2'
+     print('How much are you depositing: $')
+     deposit = gets.chomp
+     if deposit.to_i.to_s == deposit
+      overall_data = {}
+      File.open('customer.txt') do |f_obj|
+       overall_data = JSON.load(f_obj)
+       # Then append the present user details
+       overall_data[customer_acct]['Opening Balance'] += deposit.to_i
+      end
+      File.open('customer.txt', 'w') do |fil|
+       JSON.dump(overall_data, fil)
+      end
+ 
+     else
+      puts('Only numbers are allowed')
+     end
+    elsif customer_login_options == '3'
+     print('How much are you withdrawing: ')
+     withdrawal = gets.chomp
+     if withdrawal.to_i.to_s == withdrawal
+      overall_data = {}
+      File.open('customer.txt') do |f|
+       overall_data = JSON.load(f)
+       # Then append the present user details
+       overall_data[customer_acct]['Opening Balance'] -= withdrawal.to_i
+      end
+      File.open('customer.txt', 'w') do |filn|
+       JSON.dump(overall_data, filn)
+      end
+     end
+ 
+    elsif customer_login_options == '4'
+     puts('Logout Successful')
+     login = false
+     root
+    else
+     puts('Invalid entry;')
+ 
+    end
+ 
+    
+   end
+  else
+   puts('Wrong account number or password')
+   customer_login
   end
 end
 
